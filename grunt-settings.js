4e2d62e66482ae9246f8a449ff1a4a5ee74508b1
Jason <Jason.Curran@teamaol.com>
2015-07-10 15:54:48 -0600

    initial

---
+++ grunt-settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+    sassDir:          'scss/',
+    sassMainFileName: 'all',
+    cssDir:           'assets/styles/',
+    cssMainFileDir:   'assets/styles/',
+    cssMainFileName:  'all',
+    jsDir:            'assets/scripts/',
+    imgDir:           'assets/images/',
+    imgSourceDir:     'sourceimages/',
+    //hasBower: {includePaths: ['bower_components/foundation/scss']},
+
+    // sftp server
+    sftpServer:       'example.com',
+    sftpPort:         '2121',
+    sftpLogin:        'login',
+    sftpPas:          'password',
+    sftpDestination:  '/pathTo/css'
+  };
