1c69418bea3081e7a14b9f0327caefe719e758db
Reid Villeneuve <reid.villeneuve@mapquest.com>
2016-10-19 13:47:16 -0600

    MQCONT-5 - Clean-up and increases size of embed sidebar on SL

--- assets/scripts/soundslocal.js
+++ assets/scripts/soundslocal.js
@@ -1,14 +1,3 @@
-(function ($) {
-	$(function () {
-		initMobileNav();
-	});
-
-	function initMobileNav() {
-		$('#nav').mobileNav({
-			hideOnClickOutside: true,
-			menuActiveClass: 'nav-active',
-			menuOpener: '.nav-opener',
-			menuDrop: '.nav-drop'
-		});
-	}
-})(jQuery);
+(function () {
+	//Placeholder for when code is needed here later.
+})();
