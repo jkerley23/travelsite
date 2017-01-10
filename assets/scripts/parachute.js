64e68a7809534aaf81a9ae3b8a9a0d944e158758
Reid Villeneuve <reid.villeneuve@mapquest.com>
2017-01-10 10:30:30 -0700

    MQCONT-29 - Added deps and started implementation MQCONT-29 - Fullscreen functionality added MQCONT-29 - Visual tweaks MQCONT-29 - Fullscreen icons created and added MQCONT-29 - Final pre-test tweaks MQCONT-29 - Final slideshow tweaks and prod build

--- assets/scripts/parachute.js
+++ assets/scripts/parachute.js
@@ -1,37 +1,71 @@
 (function($) {
-	$(function() {
-		initCycleCarousel();
-		initTabs();
-		initPlaceholder();
-	});
+    $(function() {
+        initCycleCarousel();
+        initTabs();
+        initPlaceholder();
+    });
 
-	function initCycleCarousel() {
-		$('.slideshow').scrollAbsoluteGallery({
-			mask: '.mask',
-			slider: '.slideset',
-			slides: '.slide',
-			btnPrev: 'a.ss-arrow.arrow-prev',
-			btnNext: 'a.ss-arrow.arrow-next',
-			swipeHint: '.slide-swipe-hint',
-			pagerLinks: '.ss-pagination li',
-			stretchSlideToMask: true,
-			pauseOnHover: true,
-			maskAutoSize: false,
-			forceMaskHeightValue: '500px',
-			animSpeed: 500,
-			hideNextPrevButtonsIfTouchDevice: true,
-			swipeThreshold: 90
-		});
-	}
+    function initCycleCarousel() {
+        var slideShow = $('.slideshow'),
+            fullScreenButtonClassName = '.fullscreen-button',
+            fullScreenButton = slideShow.find(fullScreenButtonClassName),
+            slideShowOptions = {
+                mask: '.mask',
+                slider: '.slideset',
+                slides: '.slide',
+                btnPrev: 'a.ss-arrow.arrow-prev',
+                btnNext: 'a.ss-arrow.arrow-next',
+                swipeHint: '.slide-swipe-hint',
+                pagerLinks: '.ss-pagination li',
+                stretchSlideToMask: true,
+                pauseOnHover: true,
+                maskAutoSize: false,
+                forceMaskHeightValue: '500px',
+                animSpeed: 500,
+                hideNextPrevButtonsIfTouchDevice: true,
+                swipeThreshold: 90
+            },
+            modalOptions = {
+                fadeDuration: 250,
+                fadeDelay: 0.8,
+                closeClass: 'close-button'
+            },
+            slideshowFullscreenClone;
 
-	function initTabs() {
-		$('ul.tabset').contentTabs({
-			addToParent: true,
-			tabLinks: 'a'
-		});
-	}
+        slideShow.scrollAbsoluteGallery(slideShowOptions);
+        fullScreenButton.on('click', popUpSlideshow);
 
-	function initPlaceholder() {
-		$('input, textarea').placeholder();
-	}
+        function popUpSlideshow(event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            slideshowFullscreenClone = slideShow.clone()
+                .modal(modalOptions)
+                .on($.modal.OPEN, setGalleryOnModal)
+                .on($.modal.AFTER_CLOSE, clearClone);
+
+            function setGalleryOnModal() {
+                var cloneSlideshowOptions = $.extend({}, slideShowOptions, {forceMaskHeightValue: '85%'});
+
+                slideshowFullscreenClone.scrollAbsoluteGallery(cloneSlideshowOptions)
+                    .find(fullScreenButtonClassName)
+                    .hide();
+            }
+
+            function clearClone() {
+                slideshowFullscreenClone = null;
+            }
+        }
+    }
+
+    function initTabs() {
+        $('ul.tabset').contentTabs({
+            addToParent: true,
+            tabLinks: 'a'
+        });
+    }
+
+    function initPlaceholder() {
+        $('input, textarea').placeholder();
+    }
 })(jQuery);
